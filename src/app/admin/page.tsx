733c2e86677626c8da523337d3743f250f7afcd7
Mateus Azevedo <99230374+mateuscorreiaazevedo@users.noreply.github.com>
2023-06-14 23:32:37 -0300

    feat: admin painel page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,40 @@
+import Link from 'next/link'
 import React from 'react'
+
+const nav = [
+  {
+    link: '/',
+    label: 'Home',
+  },
+  {
+    link: '/admin/skill',
+    label: 'Adicionar Skill'
+  },
+  {
+    link: '/admin/project',
+    label: 'Adicionar Projeto'
+  },
+  {
+    link: '/admin/contacts',
+    label: 'Mensagens'
+  },
+]
 
 export default function Admin () {
   return (
-    <div>Admin</div>
+    <section
+      className='animate-section-2 container w-full h-screen flex items-center justify-center flex-col'
+    >
+      <h1 className='font-bold text-3xl first-letter:font-alt'>
+        Painel de Administrador
+      </h1>
+      <nav className='mt-10 flex items-center justify-between gap-2'>
+        {nav.map(item => (
+          <Link key={item.label} href={item.link} className='bg-zinc-800 px-2 py-1 font-alt text-lg hover:-translate-y-2 transition-all rounded hover:bg-violet-500'>
+            {item.label}
+          </Link>
+        ))}
+      </nav>
+    </section>
   )
 }
