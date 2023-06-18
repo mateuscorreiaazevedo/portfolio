2c807bd8021299e3366bb0057db8c0b9edf48e03
Mateus Azevedo <99230374+mateuscorreiaazevedo@users.noreply.github.com>
2023-06-18 17:22:16 -0300

    feat: added download CV button

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -5,12 +5,12 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <Section animation="section-1">
-      <div className="w-full flex flex-col lg:items-start items-center justify-center gap-4 md:gap-1">
-        <h1 className="text-5xl sm:text-7xl font-alt">Hello.</h1>
+      <div className="flex w-full flex-col items-center justify-center gap-4 md:gap-1 lg:items-start">
+        <h1 className="font-alt text-5xl sm:text-7xl">Hello.</h1>
         <h2>
           Meu nome é <span className="font-bold text-primary">Mateus Azevedo</span>.
         </h2>
-        <p className="w-80 sm:w-72 sm:text-sm md:w-80 md:text-base text-justify">
+        <p className="w-80 text-justify sm:w-72 sm:text-sm md:w-80 md:text-base">
           Desenvolvedor{' '}
           <span className="font-bold text-primary">Front-end e Mobile</span>, atuando
           desde 2018 com projetos de todos os portes. Atualmente sou freelancer como
@@ -18,12 +18,18 @@ export default function Home() {
           concentrado em performance, boas práticas de programação, responsividade e
           SEO. Além disso, estou me graduando no curso de Sistemas de Informação.
         </p>
+        <a
+          href="https://drive.google.com/uc?export=download&id=1g7xdHRqbimkhrj-eZPYKFCGfClExcgRq"
+          className="mt-2 flex h-10 w-80 items-center justify-center bg-violet-500 font-alt uppercase text-black transition-colors hover:bg-violet-600"
+        >
+          Download CV
+        </a>
       </div>
-      <div className="absolute aspect-auto flex items-center justify-center lg:shadow-none lg:top-1/2 lg:-translate-y-1/2 lg:pb-6 lg:w-80">
+      <div className="absolute flex aspect-auto items-center justify-center lg:top-1/2 lg:w-80 lg:-translate-y-1/2 lg:pb-6 lg:shadow-none">
         <Image
           src={images.myPhoto}
           alt="@mateuscorreiaazevedo"
-          className="aspect-auto lg:w-72 xl:w-80 rounded-full hidden lg:block"
+          className="hidden aspect-auto rounded-full lg:block lg:w-72 xl:w-80"
           loading="lazy"
         />
       </div>
@@ -31,7 +37,7 @@ export default function Home() {
         src={images.myPhoto}
         alt="@mateuscorreiaazevedo"
         loading="lazy"
-        className="absolute right-1/2 animate-section-2 translate-x-1/2 rounded-full w-16 aspect-auto object-contain top-20 shadow lg:hidden"
+        className="absolute right-1/2 top-20 aspect-auto w-16 translate-x-1/2 animate-section-2 rounded-full object-contain shadow lg:hidden"
       />
     </Section>
   )
