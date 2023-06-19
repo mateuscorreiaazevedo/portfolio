7a4d29e6fbc280c68c45bb0ac3f41fc5678c2b1c
Mateus Azevedo <99230374+mateuscorreiaazevedo@users.noreply.github.com>
2023-06-19 15:13:16 -0300

    refactor: adjusting modules/core

--- src/modules/core/index.ts
+++ src/modules/core/index.ts
@@ -2,5 +2,3 @@ export * from './hooks/use-set-query-string'
 export * from './utils/set-notification'
 export * from './utils/token-helper'
 export * from './service/service'
-
-export * from './components/btn-clipboard'
