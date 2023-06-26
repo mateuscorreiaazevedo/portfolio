706f10da943df00c0eca401e3c33143801f545a6
Mateus Azevedo <99230374+mateuscorreiaazevedo@users.noreply.github.com>
2023-06-26 17:47:36 -0300

    style: balls position on mobile displays

--- src/components/balls/balls.tsx
+++ src/components/balls/balls.tsx
@@ -8,22 +8,22 @@ const Balls = () => {
       <Image
         src={images.ball}
         alt=""
-        className="animate-time-4000 fixed bottom-20 left-80 -z-10 animate-section-2"
+        className="animate-time-4000 fixed bottom-20 left-20 -z-10 animate-section-2 lg:left-80"
       />
       <Image
         src={images.ball}
         alt=""
-        className="animate-time-700 fixed left-36 top-20 -z-10 animate-section-2"
+        className="animate-time-700 fixed left-10 top-20 -z-10 animate-section-2 lg:left-36"
       />
       <Image
         src={images.ball}
         alt=""
-        className="animate-time-2000 fixed right-56 top-40 -z-10 animate-section-2"
+        className="animate-time-2000 fixed right-4 top-40 -z-10 animate-section-2 lg:right-56"
       />
       <Image
         src={images.ball}
         alt=""
-        className="animate-time-1000 fixed bottom-32 right-40 -z-10 animate-section-2"
+        className="animate-time-1000 fixed bottom-32 right-24 -z-10 animate-section-2 lg:right-40"
       />
     </>
   )
