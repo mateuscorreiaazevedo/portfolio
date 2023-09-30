39b12cf62a83c43f4fcd41a2235e479420f032b4
Mateus Azevedo <99230374+mateuscorreiaazevedo@users.noreply.github.com>
2023-09-29 22:24:19 -0300

    chore: adjusting CORS on next config

--- next.config.js
+++ next.config.js
@@ -6,11 +6,10 @@ const nextConfig = {
   async headers() {
     return [
       {
-        // matching all API routes
-        source: '/api/:path*',
+        source: '/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Origin', value: 'https://mateusdev.com.br' },
           {
             key: 'Access-Control-Allow-Methods',
             value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT'
